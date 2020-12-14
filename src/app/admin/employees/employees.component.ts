5ce1540fd6b341c64bca9e977f4a6124594c19cc
SimoneBriffa <briffasimone94sr@gmail.com>
2020-12-14 12:21:33 +0100

    modifica employee

---
+++ src/app/admin/employees/employees.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/service/employee.service';
+import { EmployeeDTO } from 'src/dto/employeedto';
+
+@Component({
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css']
+})
+export class EmployeesComponent implements OnInit {
+
+  employees: EmployeeDTO[];
+  employeetoinsert: EmployeeDTO = new EmployeeDTO();
+
+  constructor(private service: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.service.getAll().subscribe(employees => this.employees = employees);
+  }
+
+  delete(employee: EmployeeDTO) {
+    this.service.delete(employee.id).subscribe(() => this.getEmployees());
+  }
+
+  update(employee: EmployeeDTO) {
+    this.service.update(employee).subscribe(() => this.getEmployees());
+  }
+
+  insert(employee: EmployeeDTO) {
+    this.service.insert(employee).subscribe(() => this.getEmployees());
+  }
+
+  clear(){
+    this.employeetoinsert = new EmployeeDTO();
+  }
+}

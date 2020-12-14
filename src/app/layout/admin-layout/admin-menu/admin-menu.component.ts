bd63e0435a2b494e433b660d7106afc7a59bf4fe
Manu785 <emanuele.pirtac@gmail.com>
2020-12-14 12:29:03 +0100

    Navbar

--- src/app/layout/admin-layout/admin-menu/admin-menu.component.ts
+++ src/app/layout/admin-layout/admin-menu/admin-menu.component.ts
@@ -9,6 +9,9 @@ import { Router } from '@angular/router';
 export class AdminMenuComponent implements OnInit {
 
   isUserCollapsed = false;
+  isEmployeeCollapsed = false;
+  isItemCollapsed = false;
+  isItemLentCollapsed = false;
   isClientCollapsed = false;
   isAccountCollapsed = false;
 
@@ -27,6 +30,24 @@ export class AdminMenuComponent implements OnInit {
     if (this.isUserCollapsed === false) {
       this.isUserCollapsed = true;
     } else { this.isUserCollapsed = false; }
+  }
+
+  employeescollapse() {
+    if (this.isEmployeeCollapsed === false) {
+      this.isEmployeeCollapsed = true;
+    } else { this.isEmployeeCollapsed = false; }
+  }
+
+  itemscollapse() {
+    if (this.isItemCollapsed === false) {
+      this.isItemCollapsed = true;
+    } else { this.isItemCollapsed = false; }
+  }
+
+  itemslentcollapse() {
+    if (this.isItemLentCollapsed === false) {
+      this.isItemLentCollapsed = true;
+    } else { this.isItemLentCollapsed = false; }
   }
 
   accountcollapse() {

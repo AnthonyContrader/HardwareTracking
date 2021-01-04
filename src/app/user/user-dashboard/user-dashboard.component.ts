704d0e2778741c1e422dd438ac0a2f4063d79081
Manu785 <emanuele.pirtac@gmail.com>
2021-01-04 14:53:10 +0100

    Profile Graphs

--- src/app/user/user-dashboard/user-dashboard.component.ts
+++ src/app/user/user-dashboard/user-dashboard.component.ts
@@ -19,15 +19,9 @@ export class UserDashboardComponent implements OnInit {
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
     localStorage.setItem('currentUser', JSON.stringify(this.user));
-  }
-
-  default(){
     if(this.user.username === this.user.employee.firstName+"_"+this.user.employee.lastName
       || this.user.password === this.user.employee.fiscalCode){
-        alert("Attenzione: il tuo username e/o password sono impostate ai valori di default." +
-        + "Consigliamo di aggiornarle.")
+        alert("\t\t\t\t\t\tAttenzione! \nIl tuo username e/o password sono impostate ai valori di default. \n\t\t\t\tConsigliamo di aggiornarle.")
       }
-    
   }
-
 }
